0dc678f4cbef3e9cd350f81ee5aaf981486f6eba
Edson Samuel Jr <samuedson@gmail.com>
2024-07-28 16:34:57 -0300

    chore: iteration exercise

--- Program.cs
+++ Program.cs
@@ -1,27 +1,18 @@
-﻿Random random = new();
-int daysUntilExpiration = random.Next(12);
-int discountPercentage = 0;
+﻿string[] orders = {
+    "B123",
+    "C234",
+    "A345",
+    "C15",
+    "B177",
+    "G3003",
+    "C235",
+    "B179"
+};
 
-if (daysUntilExpiration == 0)
+foreach (var order in orders)
 {
-    Console.WriteLine("Your subscription has expired.");
-}
-else if (daysUntilExpiration == 1)
-{
-    Console.WriteLine("Your subscription expires within a day!");
-    discountPercentage = 20;
-}
-else if (daysUntilExpiration <= 5)
-{
-    Console.WriteLine($"Your subscription expires in {daysUntilExpiration} days.");
-    discountPercentage = 10;
-}
-else if (daysUntilExpiration <= 10)
-{
-    Console.WriteLine("Your subscription will expire soon. Renew now!");
-}
-
-if (discountPercentage > 0)
-{
-    Console.WriteLine($"Renew now and save {discountPercentage}%.");
+    if (order.StartsWith('B'))
+    {
+        Console.WriteLine(order);
+    }
 }
