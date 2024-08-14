2e3481f9df0d89410e87097ee9dbd9b0d3293180
Edson Samuel Jr <samuedson@gmail.com>
2024-08-14 07:10:19 -0300

    chore(wip): code branches exercise

--- Program.cs
+++ Program.cs
@@ -1,24 +1,85 @@
-﻿string str = "The quick brown fox jumps over the lazy dog.";
+﻿using System;
 
-// convert the message into a char array
-char[] charMessage = str.ToCharArray();
+// initialize variables - graded assignments 
+int currentAssignments = 5;
 
-// Reverse the chars
-Array.Reverse(charMessage);
+int[] sophiaScores = [90, 86, 87, 98, 100];
+int[] andrewScores = [92, 89, 81, 96, 90];
+int[] emmaScores = [90, 85, 87, 98, 68];
+int[] loganScores = [90, 95, 87, 88, 96];
 
-int x = 0;
-// count the o's
-foreach (char i in charMessage)
+string[] studentNames = ["Sophia", "Andrew", "Emma", "Logan"];
+int[] studentScores = new int[10];
+
+Console.WriteLine("Student\t\tGrade\n");
+
+foreach (string currentStudent in studentNames)
 {
-    if (i == 'o')
+    if (currentStudent == "Sophia")
+        studentScores = sophiaScores;
+ 
+    else if (currentStudent == "Andrew")
+        studentScores = andrewScores;
+ 
+    else if (currentStudent == "Emma")
+        studentScores = emmaScores;
+ 
+    else if (currentStudent == "Logan")
+        studentScores = loganScores;
+
+    int sumAssignmentScores = 0;
+    decimal currentStudentGrade = 0;
+
+    foreach (int score in studentScores)
     {
-        x++;
+        sumAssignmentScores += score;
     }
+
+    currentStudentGrade = (decimal)sumAssignmentScores / currentAssignments;
+
+    string currentStudentLetterGrade = "";
+
+    if (currentStudentGrade >= 97)
+        currentStudentLetterGrade = "A+";
+
+    else if (currentStudentGrade >= 93)
+        currentStudentLetterGrade = "A";
+
+    else if (currentStudentGrade >= 90)
+        currentStudentLetterGrade = "A-";
+
+    else if (currentStudentGrade >= 87)
+        currentStudentLetterGrade = "B+";
+
+    else if (currentStudentGrade >= 83)
+        currentStudentLetterGrade = "B";
+
+    else if (currentStudentGrade >= 80)
+        currentStudentLetterGrade = "B-";
+
+    else if (currentStudentGrade >= 77)
+        currentStudentLetterGrade = "C+";
+
+    else if (currentStudentGrade >= 73)
+        currentStudentLetterGrade = "C";
+
+    else if (currentStudentGrade >= 70)
+        currentStudentLetterGrade = "C-";
+
+    else if (currentStudentGrade >= 67)
+        currentStudentLetterGrade = "D+";
+
+    else if (currentStudentGrade >= 63)
+        currentStudentLetterGrade = "D";
+
+    else if (currentStudentGrade >= 60)
+        currentStudentLetterGrade = "D-";
+
+    else
+        currentStudentLetterGrade = "F";
+
+    Console.WriteLine($"{currentStudent}\t\t{currentStudentGrade}\t{currentStudentLetterGrade}");
 }
 
-// convert it back to a string
-string newMessage = new(charMessage);
-
-// print it out
-Console.WriteLine(newMessage);
-Console.WriteLine($"'o' appears {x} times.");
+Console.WriteLine("Press the Enter key to continue");
+Console.ReadLine();
