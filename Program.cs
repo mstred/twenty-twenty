c33b4b65aa42a548ee02f0f7588b3046dfe6c6c6
Edson Samuel Jr <samuedson@gmail.com>
2024-08-20 21:36:41 -0300

    chore(wip): boolean expressions exercise

--- Program.cs
+++ Program.cs
@@ -1,110 +1,24 @@
-﻿using System;
+﻿// string value1 = " a";
+// string value2 = "A ";
 
-// initialize variables - graded assignments 
-int examAssignments = 5;
+// Console.WriteLine(value1.Trim().ToLower() == value2.Trim().ToLower());
 
-int[] sophiaScores = new int[] { 90, 86, 87, 98, 100, 94, 90 };
-int[] andrewScores = new int[] { 92, 89, 81, 96, 90, 89 };
-int[] emmaScores = new int[] { 90, 85, 87, 98, 68, 89, 89, 89 };
-int[] loganScores = new int[] { 90, 95, 87, 88, 96, 96 };
-int[] beckyScores = new int[] { 92, 91, 90, 91, 92, 92, 92 };
-int[] chrisScores = new int[] { 84, 86, 88, 90, 92, 94, 96, 98 };
-int[] ericScores = new int[] { 80, 90, 100, 80, 90, 100, 80, 90 };
-int[] gregorScores = new int[] { 91, 91, 91, 91, 91, 91, 91 };
+// Console.WriteLine("a" != "a");
+// Console.WriteLine("a" != "A");
+// Console.WriteLine(1 != 2);
 
-// Student names
-string[] studentNames = new string[] { "Sophia", "Andrew", "Emma", "Logan", "Becky", "Chris", "Eric", "Gregor" };
-int[] studentScores = new int[10];
+// string myValue = "a";
+// Console.WriteLine(myValue != "a");
 
-Console.WriteLine("Student\t\tExam Score\t\tOverall\tGrade\tExtra Credit\n");
+// Console.WriteLine(1 > 2);
+// Console.WriteLine(1 < 2);
+// Console.WriteLine(1 >= 1);
+// Console.WriteLine(1 <= 1);
 
-foreach (string currentStudent in studentNames)
-{
-    if (currentStudent == "Sophia")
-        studentScores = sophiaScores;
-    else if (currentStudent == "Andrew")
-        studentScores = andrewScores;
-    else if (currentStudent == "Emma")
-        studentScores = emmaScores;
-    else if (currentStudent == "Logan")
-        studentScores = loganScores;
-    else if (currentStudent == "Becky")
-        studentScores = beckyScores;
-    else if (currentStudent == "Chris")
-        studentScores = chrisScores;
-    else if (currentStudent == "Eric")
-        studentScores = ericScores;
-    else if (currentStudent == "Gregor")
-        studentScores = gregorScores;
-    else
-        continue;
+// string pangram = "The quick brown fox jumps over the lazy dog.";
+// Console.WriteLine(pangram.Contains("fox"));
+// Console.WriteLine(pangram.Contains("cow"));
 
-    decimal averageExamScore = 0;
-    decimal averageExtraCredit = 0;
-    decimal sumExamScores = 0;
-    decimal sumExtraCredit = 0;
-    decimal currentStudentGrade = 0;
-
-    int gradedAssignments = 0;
-    foreach (int score in studentScores)
-    {
-        gradedAssignments++;
-
-        if (gradedAssignments <= examAssignments)
-            sumExamScores += score;
-        else
-            sumExtraCredit += (decimal)score / 10;
-    }
-
-    decimal sumAssignmentScores = sumExamScores + sumExtraCredit;
-
-    averageExamScore = (decimal)sumExamScores / examAssignments;
-    averageExtraCredit = (decimal)sumExtraCredit / examAssignments;
-    currentStudentGrade = (decimal)sumAssignmentScores / examAssignments;
-
-    string currentStudentLetterGrade = "";
-
-    if (currentStudentGrade >= 97)
-        currentStudentLetterGrade = "A+";
-
-    else if (currentStudentGrade >= 93)
-        currentStudentLetterGrade = "A";
-
-    else if (currentStudentGrade >= 90)
-        currentStudentLetterGrade = "A-";
-
-    else if (currentStudentGrade >= 87)
-        currentStudentLetterGrade = "B+";
-
-    else if (currentStudentGrade >= 83)
-        currentStudentLetterGrade = "B";
-
-    else if (currentStudentGrade >= 80)
-        currentStudentLetterGrade = "B-";
-
-    else if (currentStudentGrade >= 77)
-        currentStudentLetterGrade = "C+";
-
-    else if (currentStudentGrade >= 73)
-        currentStudentLetterGrade = "C";
-
-    else if (currentStudentGrade >= 70)
-        currentStudentLetterGrade = "C-";
-
-    else if (currentStudentGrade >= 67)
-        currentStudentLetterGrade = "D+";
-
-    else if (currentStudentGrade >= 63)
-        currentStudentLetterGrade = "D";
-
-    else if (currentStudentGrade >= 60)
-        currentStudentLetterGrade = "D-";
-
-    else
-        currentStudentLetterGrade = "F";
-
-    Console.WriteLine($"{currentStudent}\t\t{averageExamScore}\t\t\t{currentStudentGrade}\t{currentStudentLetterGrade}\t{Math.Floor(averageExamScore)} ({averageExtraCredit} pts)");
-}
-
-Console.WriteLine("Press the Enter key to continue");
-Console.ReadLine();
+string pangram = "The quick brown fox jumps over the lazy dog.";
+Console.WriteLine(!pangram.Contains("fox"));
+Console.WriteLine(!pangram.Contains("cow"));
