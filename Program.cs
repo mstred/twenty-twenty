183ea6f341b8ee62b39b6480d7db23f848b891f0
Edson Samuel Jr <samuedson@gmail.com>
2024-08-25 18:12:23 -0300

    chore: do-while exercise 1

--- Program.cs
+++ Program.cs
@@ -1,9 +1,21 @@
-﻿for (int i = 1; i <= 100; i++)
-    if (i % 3 == 0 && i % 5 == 0)
-        Console.WriteLine($"{i} - FizzBuzz");
-    else if (i % 3 == 0)
-        Console.WriteLine($"{i} - Fizz");
-    else if (i % 5 == 0)
-        Console.WriteLine($"{i} - Buzz");
-    else
-        Console.WriteLine(i);
+﻿var attack = new Random();
+var heroHealth = 10;
+var monsterHealth = 10;
+
+while (heroHealth > 0 || monsterHealth > 0)
+{
+    var heroAttack = attack.Next(1, 11);
+    monsterHealth -= heroAttack;
+
+    Console.WriteLine($"Monster was damaged and lost {heroAttack} and now has {monsterHealth}.");
+
+    if (monsterHealth <= 0)
+        break;
+
+    var monsterAttack = attack.Next(1, 11);
+    heroHealth -= monsterAttack;
+
+    Console.WriteLine($"Hero was damaged and lost {monsterAttack} and now has {heroHealth}.");
+}
+
+Console.WriteLine($"{((heroHealth > monsterHealth) ? "Hero" : "Monster")} wins!");
