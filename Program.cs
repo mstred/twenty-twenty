12e68fe10b2770478b6664bcaa432df54c7af6c7
Edson Samuel Jr <samuedson@gmail.com>
2024-10-20 12:00:23 -0300

    chore(wip): typecasting and conversion exercise

--- Program.cs
+++ Program.cs
@@ -1,27 +1,53 @@
-﻿Console.WriteLine("Signed integral types:");
+﻿// int first = 2;
+// string second = "4";
+// string result = first + second;
+// Console.WriteLine(result);
 
-Console.WriteLine($"sbyte  : {sbyte.MinValue} to {sbyte.MaxValue}");
-Console.WriteLine($"short  : {short.MinValue} to {short.MaxValue}");
-Console.WriteLine($"int    : {int.MinValue} to {int.MaxValue}");
-Console.WriteLine($"long   : {long.MinValue} to {long.MaxValue}");
+// int myInt = 3;
+// Console.WriteLine($"int: {myInt}");
 
-Console.WriteLine("Unsigned integral types:");
+// decimal myDecimal = myInt;
+// Console.WriteLine($"decimal: {myDecimal}");
 
-Console.WriteLine($"byte  : {byte.MinValue} to {byte.MaxValue}");
-Console.WriteLine($"ushort  : {ushort.MinValue} to {ushort.MaxValue}");
-Console.WriteLine($"uint    : {uint.MinValue} to {uint.MaxValue}");
-Console.WriteLine($"ulong   : {ulong.MinValue} to {ulong.MaxValue}");
+// decimal myDecimal = 3.14m;
+// Console.WriteLine($"decimal: {myDecimal}");
 
-Console.WriteLine("Unsigned integral types:");
+// int myInt = (int)myDecimal;
+// Console.WriteLine($"int: {myInt}");
 
-Console.WriteLine($"byte  : {byte.MinValue} to {byte.MaxValue}");
-Console.WriteLine($"ushort  : {ushort.MinValue} to {ushort.MaxValue}");
-Console.WriteLine($"uint    : {uint.MinValue} to {uint.MaxValue}");
-Console.WriteLine($"ulong   : {ulong.MinValue} to {ulong.MaxValue}");
+// decimal myDecimal = 1.23456789m;
+// float myFloat = (float)myDecimal;
 
-Console.WriteLine("Floating point types:");
-Console.WriteLine($"float   : {float.MinValue} to {float.MaxValue} (with ~6-9 digits of precision)");
-Console.WriteLine($"double  : {double.MinValue} to {double.MaxValue} (with ~15-17 digits of precision)");
-Console.WriteLine($"decimal : {decimal.MinValue} to {decimal.MaxValue} (with ~28-29 digits of precision)");
+// Console.WriteLine($"Decimal: {myDecimal}");
+// Console.WriteLine($"Float  : {myFloat}");
 
-int[] data = new int[3];
+// int first = 5;
+// int second = 7;
+// string message = first.ToString() + second.ToString();
+// Console.WriteLine(message);
+
+// string first = "5";
+// string second = "7";
+// int sum = int.Parse(first) + int.Parse(second);
+// Console.WriteLine(sum);
+
+// string value1 = "5a";
+// string value2 = "7";
+// int result = Convert.ToInt32(value1) * Convert.ToInt32(value2);
+// Console.WriteLine(result);
+
+// int value1 = (int)1.5m;
+// Console.WriteLine(value1);
+
+// int value2 = Convert.ToInt32(1.5m);
+// Console.WriteLine(value2);
+
+string value1 = "bad";
+int result;
+if (int.TryParse(value1, out result))
+    Console.WriteLine($"Measurement: {result}");
+else
+    Console.WriteLine("Unable to report the measurement.");
+
+if (result > 0)
+    Console.WriteLine($"Measurement (w/ offset): {50 + result}");
