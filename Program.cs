e344464c74c6c63f11628df1e9c413198ca78292
Edson Samuel Jr <samuedson@gmail.com>
2024-10-24 22:09:05 -0300

    chore: typecasting and conversion challenge 2

--- Program.cs
+++ Program.cs
@@ -1,75 +1,12 @@
-﻿// int first = 2;
-// string second = "4";
-// string result = first + second;
-// Console.WriteLine(result);
+﻿int value1 = 11;
+decimal value2 = 6.2m;
+float value3 = 4.3f;
 
-// int myInt = 3;
-// Console.WriteLine($"int: {myInt}");
+int result1 = Convert.ToInt32(value1 / value2);
+Console.WriteLine($"Divide value1 by value2, display the result as an int: {result1}");
 
-// decimal myDecimal = myInt;
-// Console.WriteLine($"decimal: {myDecimal}");
+decimal result2 = value2 / (decimal)value3;
+Console.WriteLine($"Divide value2 by value3, display the result as a decimal: {result2}");
 
-// decimal myDecimal = 3.14m;
-// Console.WriteLine($"decimal: {myDecimal}");
-
-// int myInt = (int)myDecimal;
-// Console.WriteLine($"int: {myInt}");
-
-// decimal myDecimal = 1.23456789m;
-// float myFloat = (float)myDecimal;
-
-// Console.WriteLine($"Decimal: {myDecimal}");
-// Console.WriteLine($"Float  : {myFloat}");
-
-// int first = 5;
-// int second = 7;
-// string message = first.ToString() + second.ToString();
-// Console.WriteLine(message);
-
-// string first = "5";
-// string second = "7";
-// int sum = int.Parse(first) + int.Parse(second);
-// Console.WriteLine(sum);
-
-// string value1 = "5a";
-// string value2 = "7";
-// int result = Convert.ToInt32(value1) * Convert.ToInt32(value2);
-// Console.WriteLine(result);
-
-// int value1 = (int)1.5m;
-// Console.WriteLine(value1);
-
-// int value2 = Convert.ToInt32(1.5m);
-// Console.WriteLine(value2);
-
-// string value1 = "bad";
-// int result;
-// if (int.TryParse(value1, out result))
-//     Console.WriteLine($"Measurement: {result}");
-// else
-//     Console.WriteLine("Unable to report the measurement.");
-
-// if (result > 0)
-//     Console.WriteLine($"Measurement (w/ offset): {50 + result}");
-
-using System.Globalization;
-
-CultureInfo.CurrentCulture = new CultureInfo("en-US");
-
-string[] values = { "12.3", "45", "ABC", "11", "DEF" };
-
-string message = "";
-decimal total = 0;
-
-foreach (string value in values)
-{
-    bool isValidNumber = decimal.TryParse(value, out decimal numericValue);
-
-    if (isValidNumber)
-        total += numericValue;
-    else
-        message += value;
-}
-
-Console.WriteLine($"Message: {message}");
-Console.WriteLine($"Total: {total}");
+float result3 = value3 / value1;
+Console.WriteLine($"Divide value3 by value1, display the result as a float: {result3}");
