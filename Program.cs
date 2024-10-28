8fdc08919283460dc39795dfdfc5d449f689e178
Edson Samuel Jr <samuedson@gmail.com>
2024-10-28 12:08:03 -0300

    chore: array operations challenge 1

--- Program.cs
+++ Program.cs
@@ -1,56 +1,17 @@
-﻿// string[] pallets = ["B14", "A11", "B12", "A13"];
+﻿string pangram = "The quick brown fox jumps over the lazy dog";
 
-// Console.WriteLine("Sorted...");
-// Array.Sort(pallets);
-// foreach (var pallet in pallets)
-// {
-//     Console.WriteLine($"-- {pallet}");
-// }
+string[] words = pangram.Split(" ");
+string[] reversedWords = new string[words.Length];
+int index = 0;
 
-// Console.WriteLine("\nReversed...");
-// Array.Reverse(pallets);
-// foreach (var pallet in pallets)
-// {
-//     Console.WriteLine($"-- {pallet}");
-// }
+foreach (var word in words)
+{
+    char[] letters = word.ToCharArray();
+    Array.Reverse(letters);
 
-// string[] pallets = ["B14", "A11", "B12", "A13"];
+    reversedWords[index++] = new string(letters);
+}
 
-// // Console.WriteLine($"Before: {pallets[0].ToLower()}");
-// Array.Clear(pallets, 0, 2);
+string output = String.Join(" ", reversedWords);
 
-// // if (pallets[0] != null)
-// //     Console.WriteLine($"After: {pallets[0].ToLower()}");
-
-// Console.WriteLine($"\nClearing 2 ... count: {pallets.Length}");
-
-// foreach (var pallet in pallets)
-//     Console.WriteLine($"-- {pallet}");
-
-// Array.Resize(ref pallets, 6);
-// Console.WriteLine($"\nResizing 6 ... count: {pallets.Length}");
-
-// pallets[4] = "C01";
-// pallets[5] = "C02";
-
-// foreach (var pallet in pallets)
-//     Console.WriteLine($"-- {pallet}");
-
-// Array.Resize(ref pallets, 3);
-// Console.WriteLine($"\nResizing 3 ... count: {pallets.Length}");
-
-// foreach (var pallet in pallets)
-//     Console.WriteLine($"-- {pallet}");
-
-string value = "abc123";
-char[] valueArray = value.ToCharArray();
-
-Array.Reverse(valueArray);
-
-// string result = new(valueArray);
-string result = String.Join(",", valueArray);
-Console.WriteLine(result);
-
-string[] items = result.Split(',');
-foreach (var item in items)
-    Console.WriteLine(item);
+Console.WriteLine(output);
