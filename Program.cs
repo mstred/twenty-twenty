8efa105e00e844d69f803fc5f847e67f8e23c7d5
Edson Samuel Jr <samuedson@gmail.com>
2024-10-28 11:44:11 -0300

    chore: array operations exercise 2 - clear/resize

--- Program.cs
+++ Program.cs
@@ -1,15 +1,43 @@
-﻿string[] pallets = ["B14", "A11", "B12", "A13"];
+﻿// string[] pallets = ["B14", "A11", "B12", "A13"];
 
-Console.WriteLine("Sorted...");
-Array.Sort(pallets);
-foreach (var pallet in pallets)
-{
-    Console.WriteLine($"-- {pallet}");
-}
+// Console.WriteLine("Sorted...");
+// Array.Sort(pallets);
+// foreach (var pallet in pallets)
+// {
+//     Console.WriteLine($"-- {pallet}");
+// }
 
-Console.WriteLine("\nReversed...");
-Array.Reverse(pallets);
+// Console.WriteLine("\nReversed...");
+// Array.Reverse(pallets);
+// foreach (var pallet in pallets)
+// {
+//     Console.WriteLine($"-- {pallet}");
+// }
+
+string[] pallets = ["B14", "A11", "B12", "A13"];
+
+// Console.WriteLine($"Before: {pallets[0].ToLower()}");
+Array.Clear(pallets, 0, 2);
+
+// if (pallets[0] != null)
+//     Console.WriteLine($"After: {pallets[0].ToLower()}");
+
+Console.WriteLine($"\nClearing 2 ... count: {pallets.Length}");
+
 foreach (var pallet in pallets)
-{
     Console.WriteLine($"-- {pallet}");
-}
+
+Array.Resize(ref pallets, 6);
+Console.WriteLine($"\nResizing 6 ... count: {pallets.Length}");
+
+pallets[4] = "C01";
+pallets[5] = "C02";
+
+foreach (var pallet in pallets)
+    Console.WriteLine($"-- {pallet}");
+
+Array.Resize(ref pallets, 3);
+Console.WriteLine($"\nResizing 3 ... count: {pallets.Length}");
+
+foreach (var pallet in pallets)
+    Console.WriteLine($"-- {pallet}");
