eea277f495ea1fa46f7a8326d856c4c8699beaaa
Edson Samuel Jr <samuedson@gmail.com>
2024-11-03 16:49:37 -0300

    chore: modify strings challenge

--- Program.cs
+++ Program.cs
@@ -1,9 +1,12 @@
-﻿// string data = "12345John Smith          5000  3  ";
-// string updatedData = data.Remove(5, 20);
-// Console.WriteLine(updatedData);
+﻿const string input = "<div><h2>Widgets &trade;</h2><span>5000</span></div>";
 
-string message = "This--is--ex-amp-le--da-ta";
-message = message.Replace("--", " ");
-message = message.Replace("-", "");
+string quantity = "";
+string output = "";
 
-Console.WriteLine(message);
+int spanContentIndex = input.IndexOf("<span>") + 6;
+
+quantity = input.Substring(spanContentIndex, input.IndexOf("</span>") - spanContentIndex);
+output = input.Replace("&trade;", "&reg;").Replace("<div>", "").Replace("</div>", "");
+
+Console.WriteLine($"Quantity: {quantity}");
+Console.WriteLine($"Output: {output}");
