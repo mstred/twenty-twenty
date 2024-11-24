2c967a05c4688fcb04759178846f2c2d942f0074
Edson Samuel Jr <samuedson@gmail.com>
2024-11-24 19:35:27 -0300

    chore(write-your-first-method): base challenge project

--- Program.cs
+++ Program.cs
@@ -1,66 +1,14 @@
-﻿/*
-if ipAddress consists of 4 numbers
-and
-if each ipAddress number has no leading zeroes
-and
-if each ipAddress number is in range 0 - 255
+﻿Random random = new Random();
+int luck = random.Next(100);
 
-then ipAddress is valid
+string[] text = {"You have much to", "Today is a day to", "Whatever work you do", "This is an ideal time to"};
+string[] good = {"look forward to.", "try new things!", "is likely to succeed.", "accomplish your dreams!"};
+string[] bad = {"fear.", "avoid major decisions.", "may have unexpected outcomes.", "re-evaluate your life."};
+string[] neutral = {"appreciate.", "enjoy time with friends.", "should align with your values.", "get in tune with nature."};
 
-else ipAddress is invalid
-*/
-
-string[] ipv4Input = ["107.31.1.5", "255.0.0.255", "555..0.555", "255...255"];
-bool validLength;
-bool validZeroes;
-bool validRange;
-
-foreach (string ip in ipv4Input)
+Console.WriteLine("A fortune teller whispers the following words:");
+string[] fortune = (luck > 75 ? good : (luck < 25 ? bad : neutral));
+for (int i = 0; i < 4; i++) 
 {
-    ValidateLength(ip);
-    ValidateZeroes(ip);
-    ValidateRange(ip);
-
-    if (validLength && validZeroes && validRange)
-        Console.WriteLine($"ip is a valid IPv4 address");
-    else
-        Console.WriteLine($"ip is an invalid IPv4 address");
-}
-
-void ValidateLength(string ip)
-{
-    validLength = GetSplitAddress(ip).Length == 4;
-}
-
-void ValidateZeroes(string ip)
-{
-    foreach (string number in GetSplitAddress(ip))
-    {
-        validZeroes = !(number.Length > 1 && number.StartsWith('0'));
-        if (!validZeroes)
-            return;
-    }
-
-    validZeroes = true;
-}
-
-void ValidateRange(string ip)
-{
-    foreach (string number in GetSplitAddress(ip))
-    {
-        bool validParse = int.TryParse(number, out int value);
-        if (!validParse || value < 0 || value > 255)
-        {
-            validRange = false;
-            return;
-        }
-    }
-
-    validRange = true;
-}
-
-string[] GetSplitAddress(string ip)
-{
-    return ip.Split('.', StringSplitOptions.RemoveEmptyEntries
-                                & StringSplitOptions.TrimEntries);
+    Console.Write($"{text[i]} {fortune[i]} ");
 }
