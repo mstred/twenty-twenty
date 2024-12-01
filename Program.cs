d90e2361efa602d9af4ba3a3959810d8aa7be42e
Edson Samuel Jr <samuedson@gmail.com>
2024-12-01 16:38:34 -0300

    chore: "use parameters in methods" exercise 1

--- Program.cs
+++ Program.cs
@@ -1,24 +1,23 @@
-﻿Random random = new();
-int luck;
+﻿int[] schedule = [800, 1200, 1600, 2000];
 
-string[] text = ["You have much to", "Today is a day to", "Whatever work you do", "This is an ideal time to"];
-string[] good = ["look forward to.", "try new things!", "is likely to succeed.", "accomplish your dreams!"];
-string[] bad = ["fear.", "avoid major decisions.", "may have unexpected outcomes.", "re-evaluate your life."];
-string[] neutral = ["appreciate.", "enjoy time with friends.", "should align with your values.", "get in tune with nature."];
-
-luck = random.Next(100);
-TellFortune();
-
-luck = random.Next(100);
-TellFortune();
-
-void TellFortune()
+void DisplayAdjustedTimes(int[] times, int currentGMT, int newGMT)
 {
-    Console.WriteLine("A fortune teller whispers the following words:");
-    string[] fortune = (luck > 75) ? good : ((luck < 25) ? bad : neutral);
+    int diff = 0;
+    int currentGMTAbs = Math.Abs(currentGMT);
+    int newGMTAbs = Math.Abs(newGMT);
 
-    for (int i = 0; i < 4; i++)
-        Console.Write($"{text[i]} {fortune[i]} ");
+    if (currentGMTAbs > 12 || newGMTAbs > 12)
+        Console.WriteLine("Invalid GMT");
+    else if ((currentGMT <= 0 && newGMT <= 0) || (currentGMT >= 0 && newGMT >= 0))
+        diff = 100 * (currentGMTAbs - newGMTAbs);
+    else
+        diff = 100 * (currentGMTAbs + newGMTAbs);
 
-    Console.WriteLine('\n');
+    foreach (var time in times)
+    {
+        int newTime = (time + diff) % 2400;
+        Console.WriteLine($"{time} -> {newTime}");
+    }
 }
+
+DisplayAdjustedTimes(schedule, 6, -6);
