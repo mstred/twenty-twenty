4dfb848332b16e934267e07c10aff131379e461f
Edson Samuel Jr <samuedson@gmail.com>
2024-12-01 17:49:01 -0300

    chore: use named arguments

--- Program.cs
+++ Program.cs
@@ -1,21 +1,49 @@
-﻿string status = "Healthy";
+﻿string[] guestList = ["Rebecca", "Nadia", "Noor", "Jonte"];
+string[] rsvps = new string[10];
+int count = 0;
 
-// Console.WriteLine($"Start: {status}");
-// SetHealth(status, false);
-// Console.WriteLine($"End: {status}");
+RSVP("Rebecca", 1, "none", true);
+RSVP("Nadia", 2, "Nuts", true);
+RSVP(name: "Linh", partySize: 2, allergies: "none", inviteOnly: false);
+RSVP(name: "Tony", inviteOnly: true, allergies: "Jackfruit", partySize: 1);
+RSVP("Noor", 4, "none", false);
+RSVP("Jonte", 2, "Stone fruit", false);
+ShowRSVPs();
 
-// void SetHealth(string status, bool isHealthy)
-// {
-//     status = isHealthy ? "Healthy" : "Unhealthy";
-//     Console.WriteLine($"Middle: {status}");
-// }
-
-Console.WriteLine($"Start: {status}");
-SetHealth(false);
-Console.WriteLine($"End: {status}");
-
-void SetHealth(bool isHealthy)
+void RSVP(string name, int partySize, string allergies, bool inviteOnly)
 {
-    status = isHealthy ? "Healthy" : "Unhealthy";
-    Console.WriteLine($"Middle: {status}");
+    if (inviteOnly)
+    {
+        // search guestList before adding rsvp
+        bool found = false;
+
+        foreach (var guest in guestList)
+        {
+            if (guest.Equals(name))
+            {
+                found = true;
+                break;
+            }
+        }
+
+        if (!found)
+        {
+            Console.WriteLine($"Sorry, {name} is not on the guest list");
+            return;
+        }
+    }
+
+    rsvps[count] = $"Name: {name}, \tParty Size: {partySize}, \tAllergies: {allergies}";
+    count++;
+}
+
+void ShowRSVPs()
+{
+    Console.WriteLine("\nTotal RSVPs:");
+
+    foreach (var rsvp in rsvps)
+    {
+        if (!string.IsNullOrEmpty(rsvp))
+            Console.WriteLine(rsvp);
+    }
 }
