c4803b17f224aa69844611c936d53dc3d65e1491
Edson Samuel Jr <samuedson@gmail.com>
2024-12-01 17:25:31 -0300

    chore: use reference parameters

--- Program.cs
+++ Program.cs
@@ -1,12 +1,18 @@
-﻿int a = 3;
-int b = 4;
-int c = 0;
+﻿int[] array = [1, 2, 3, 4, 5];
 
-Multiply(a, b, c);
-Console.WriteLine($"global statement: {a} x {b} = {c}");
+PrintArray(array);
+Clear(array);
+PrintArray(array);
 
-void Multiply(int a, int b, int c)
+void PrintArray(int[] array)
 {
-    c = a * b;
-    Console.WriteLine($"inside Multiply method: {a} x {b} = {c}");
+    foreach (var a in array)
+        Console.Write($"{a} ");
+    Console.WriteLine();        
+}
+
+void Clear(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        array[i] = 0;
 }
