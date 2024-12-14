11853518ea1a40ddb5207f447ea625574d94b5a8
Edson Samuel Jr <samuedson@gmail.com>
2024-12-14 16:16:56 -0300

    chore: return numbers from methods

--- Program.cs
+++ Program.cs
@@ -1,27 +1,17 @@
-﻿double total = 0;
-double minimumSpend = 30.00;
+﻿double usd = 23.73;
+int vnd = UsdToVnd(usd);
 
-double[] items = [15.97, 3.50, 12.25, 22.99, 10.98];
-double[] discounts = [0.30, 0.00, 0.10, 0.20, 0.50];
+Console.WriteLine($"${usd} USD = ${vnd} VND");
+Console.WriteLine($"${vnd} VND = ${VndToUsd(vnd)} USD");
 
-for (var i = 0; i < items.Length; i++)
-    total += GetDiscountedPrice(i);
-
-if (TotalMeetsMinimum())
-    total -= 5.00;
-
-Console.WriteLine($"Total: ${FormatDecimal(total)}");
-
-double GetDiscountedPrice(int itemIndex)
+int UsdToVnd(double usd)
 {
-    // Calculate the discounted price of the item
-    return items[itemIndex] * (1 - discounts[itemIndex]);
+    int rate = 23500;
+    return (int) (rate * usd);
 }
 
-bool TotalMeetsMinimum() => total >= minimumSpend; // Check if the total meets the minimum
-
-string FormatDecimal(double input)
+double VndToUsd(int vnd)
 {
-    // Format the double so only 2 decimal places are displayed
-    return input.ToString()[..5];
+    double rate = 23500;
+    return vnd / rate;
 }
