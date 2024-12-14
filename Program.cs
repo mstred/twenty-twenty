2b19076a6237476a5ac08cf15018414aa22e5819
Edson Samuel Jr <samuedson@gmail.com>
2024-12-14 16:31:22 -0300

    chore: return strings from methods

--- Program.cs
+++ Program.cs
@@ -1,17 +1,25 @@
-﻿double usd = 23.73;
-int vnd = UsdToVnd(usd);
-
-Console.WriteLine($"${usd} USD = ${vnd} VND");
-Console.WriteLine($"${vnd} VND = ${VndToUsd(vnd)} USD");
-
-int UsdToVnd(double usd)
+﻿string ReverseWord(string word)
 {
-    int rate = 23500;
-    return (int) (rate * usd);
+    string result = "";
+
+    for (int i = word.Length - 1; i >= 0; i--)
+        result += word[i];
+
+    return result;
 }
 
-double VndToUsd(int vnd)
+string input = "there are snakes in the zoo";
+
+Console.WriteLine(input);
+Console.WriteLine(ReverseSentence(input));
+
+string ReverseSentence(string input)
 {
-    double rate = 23500;
-    return vnd / rate;
+    string result = "";
+    string[] words = input.Split(" ");
+
+    foreach (string word in words)
+        result += $"{ReverseWord(word)} ";
+
+    return result.Trim();
 }
