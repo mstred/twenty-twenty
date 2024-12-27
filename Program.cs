ee7b7c1769604bb1457757c24a28b9c4ef9c62a8
Edson Samuel Jr <samuedson@gmail.com>
2024-12-27 16:52:24 -0300

    chore: "create c# methods that return values" challenge

--- Program.cs
+++ Program.cs
@@ -1,10 +1,8 @@
-﻿Random random = new Random();
+﻿Random random = new();
 
 Console.WriteLine("Would you like to play? (Y/N)");
 if (ShouldPlay()) 
-{
     PlayGame();
-}
 
 void PlayGame() 
 {
@@ -12,14 +10,17 @@ void PlayGame()
 
     while (play) 
     {
-        var target;
-        var roll;
+        var target = random.Next(1, 6);
+        var roll = random.Next(1, 7);
 
         Console.WriteLine($"Roll a number greater than {target} to win!");
         Console.WriteLine($"You rolled a {roll}");
-        Console.WriteLine(WinOrLose());
+        Console.WriteLine(WinOrLose(roll, target));
         Console.WriteLine("\nPlay again? (Y/N)");
 
         play = ShouldPlay();
     }
 }
+
+bool ShouldPlay() => (Console.ReadLine() ?? "").ToUpper() == "Y";
+string WinOrLose(int roll, int target) => $"You {(roll > target ? "win" : "lose")}!";
