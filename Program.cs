098462765a6090fb4e3b4cc335b93ce5ede32f4b
Edson Samuel Jr <samuedson@gmail.com>
2025-01-04 16:35:26 -0300

    chore(plan-petting-zoo-visit): base guided project from microsoft learning repo

--- Program.cs
+++ Program.cs
@@ -1,26 +1,8 @@
-﻿Random random = new();
+﻿using System;
 
-Console.WriteLine("Would you like to play? (Y/N)");
-if (ShouldPlay()) 
-    PlayGame();
-
-void PlayGame() 
+string[] pettingZoo = 
 {
-    var play = true;
-
-    while (play) 
-    {
-        var target = random.Next(1, 6);
-        var roll = random.Next(1, 7);
-
-        Console.WriteLine($"Roll a number greater than {target} to win!");
-        Console.WriteLine($"You rolled a {roll}");
-        Console.WriteLine(WinOrLose(roll, target));
-        Console.WriteLine("\nPlay again? (Y/N)");
-
-        play = ShouldPlay();
-    }
-}
-
-bool ShouldPlay() => (Console.ReadLine() ?? "").ToUpper() == "Y";
-string WinOrLose(int roll, int target) => $"You {(roll > target ? "win" : "lose")}!";
+    "alpacas", "capybaras", "chickens", "ducks", "emus", "geese", 
+    "goats", "iguanas", "kangaroos", "lemurs", "llamas", "macaws", 
+    "ostriches", "pigs", "ponies", "rabbits", "sheep", "tortoises",
+};
