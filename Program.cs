9aa6b18b63cc80fef48a807a5c6caa5ce8110eb3
Edson Samuel Jr <samuedson@gmail.com>
2025-01-05 12:42:05 -0300

    chore(mini-game): base challenge project from microsoft learning repo

--- Program.cs
+++ Program.cs
@@ -1,76 +1,117 @@
-﻿/*
-You're developing an application for the Contoso Petting Zoo that coordinates school visits. The Contoso Petting Zoo is home to 18 different species of animals. At the zoo, visiting students are assigned to groups, and each group has a set of animals assigned to it. After visiting their set of animals, the students will rotate groups until they've seen all the animals at the petting zoo.
+﻿using System;
 
-By default, the students are divided into 6 groups. However, there are some classes that have a small or large number of students, so the number of groups must be adjusted accordingly. The animals should also be randomly assigned to each group, so as to keep the experience unique.
+Random random = new Random();
+Console.CursorVisible = false;
+int height = Console.WindowHeight - 1;
+int width = Console.WindowWidth - 5;
+bool shouldExit = false;
 
-The design specification for the Contoso Petting Zoo application is as follows:
+// Console position of the player
+int playerX = 0;
+int playerY = 0;
 
-- There will be three visiting schools
-    - School A has six visiting groups (the default number) ✅
-    - School B has three visiting groups ✅
-    - School C has two visiting groups ✅
+// Console position of the food
+int foodX = 0;
+int foodY = 0;
 
-- For each visiting school, perform the following tasks
-    - Randomize the animals ✅
-    - Assign the animals to the correct number of groups ✅
-    - Print the school name ✅
-    - Print the animal groups ✅
-*/
+// Available player and food strings
+string[] states = {"('-')", "(^-^)", "(X_X)"};
+string[] foods = {"@@@@@", "$$$$$", "#####"};
 
-string[] pettingZoo = 
-[
-    "alpacas", "capybaras", "chickens", "ducks", "emus", "geese", 
-    "goats", "iguanas", "kangaroos", "lemurs", "llamas", "macaws", 
-    "ostriches", "pigs", "ponies", "rabbits", "sheep", "tortoises",
-];
+// Current player string displayed in the Console
+string player = states[0];
 
-Console.Clear();
-PlanSchoolVisit("School A");
-PlanSchoolVisit("School B", 3);
-PlanSchoolVisit("School C", 2);
+// Index of the current food
+int food = 0;
 
-void PlanSchoolVisit(string schoolName, int groups = 6)
+InitializeGame();
+while (!shouldExit) 
 {
-    RandomizeAnimals();
-    string[,] group = AssignGroup(groups);
-    Console.WriteLine(schoolName);
-    PrintGroup(group);
-    Console.WriteLine();
+    Move();
 }
 
-void RandomizeAnimals()
+// Returns true if the Terminal was resized 
+bool TerminalResized() 
 {
-    Random random = new();
-    var pettingZooLength = pettingZoo.Length;
+    return height != Console.WindowHeight - 1 || width != Console.WindowWidth - 5;
+}
 
-    for (int i = 0; i < pettingZooLength; i++)
+// Displays random food at a random location
+void ShowFood() 
+{
+    // Update food to a random index
+    food = random.Next(0, foods.Length);
+
+    // Update food position to a random location
+    foodX = random.Next(0, width - player.Length);
+    foodY = random.Next(0, height - 1);
+
+    // Display the food at the location
+    Console.SetCursorPosition(foodX, foodY);
+    Console.Write(foods[food]);
+}
+
+// Changes the player to match the food consumed
+void ChangePlayer() 
+{
+    player = states[food];
+    Console.SetCursorPosition(playerX, playerY);
+    Console.Write(player);
+}
+
+// Temporarily stops the player from moving
+void FreezePlayer() 
+{
+    System.Threading.Thread.Sleep(1000);
+    player = states[0];
+}
+
+// Reads directional input from the Console and moves the player
+void Move() 
+{
+    int lastX = playerX;
+    int lastY = playerY;
+    
+    switch (Console.ReadKey(true).Key) 
     {
-        int r = random.Next(i, pettingZooLength);
-        (pettingZoo[r], pettingZoo[i]) = (pettingZoo[i], pettingZoo[r]);
+        case ConsoleKey.UpArrow:
+            playerY--; 
+            break;
+		case ConsoleKey.DownArrow: 
+            playerY++; 
+            break;
+		case ConsoleKey.LeftArrow:  
+            playerX--; 
+            break;
+		case ConsoleKey.RightArrow: 
+            playerX++; 
+            break;
+		case ConsoleKey.Escape:     
+            shouldExit = true; 
+            break;
     }
-}
 
-string[,] AssignGroup(int groups = 6)
-{
-    int start = 0;
-    string[,] result = new string[groups, pettingZoo.Length/groups];
-
-    for (int i = 0; i < groups; i++)
-        for (int j = 0; j < result.GetLength(1); j++)
-            result[i, j] = pettingZoo[start++];
-
-    return result;
-}
-
-void PrintGroup(string[,] group)
-{
-    for (int i = 0; i < group.GetLength(0); i++)
+    // Clear the characters at the previous position
+    Console.SetCursorPosition(lastX, lastY);
+    for (int i = 0; i < player.Length; i++) 
     {
-        Console.Write($"Group {i + 1}: ");
-
-        for (int j = 0; j < group.GetLength(1); j++)
-            Console.Write($"{group[i, j]} ");
-
-        Console.WriteLine();
+        Console.Write(" ");
     }
+
+    // Keep player position within the bounds of the Terminal window
+    playerX = (playerX < 0) ? 0 : (playerX >= width ? width : playerX);
+    playerY = (playerY < 0) ? 0 : (playerY >= height ? height : playerY);
+
+    // Draw the player at the new location
+    Console.SetCursorPosition(playerX, playerY);
+    Console.Write(player);
+}
+
+// Clears the console, displays the food and player
+void InitializeGame() 
+{
+    Console.Clear();
+    ShowFood();
+    Console.SetCursorPosition(0, 0);
+    Console.Write(player);
 }
