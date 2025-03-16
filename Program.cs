d92a85ab3a17d9a243146c32c3d74655c0620789
Edson Samuel Jr <samuedson@gmail.com>
2025-03-16 16:52:32 -0300

    chore: monitor variable state

--- Program.cs
+++ Program.cs
@@ -1,69 +1,42 @@
-﻿int productCount = 2000;
-string[,] products = new string[productCount, 2];
+﻿string? readResult;
+int startIndex = 0;
+bool goodEntry = false;
 
-LoadProducts(products, productCount);
+int[] numbers = [1, 2, 3, 4, 5];
 
-for (int i = 0; i < productCount; i++)
+// Display the array to the console.
+Console.Clear();
+Console.Write("\n\rThe 'numbers' array contains: { ");
+foreach (int number in numbers)
+    Console.Write($"{number} ");
+
+// To calculate a sum of array elements, 
+//  prompt the user for the starting element number.
+Console.WriteLine($"}}\n\r\n\rTo sum values 'n' through 5, enter a value for 'n':");
+while (goodEntry == false)
 {
-    string result;
-    result = Process1(products, i);
+    readResult = Console.ReadLine();
+    goodEntry = int.TryParse(readResult, out startIndex);
 
-    if (result != "obsolete")
+    if (startIndex > 5)
     {
-        _ = Process2(products, i);
+        goodEntry = false;
+        Console.WriteLine("\n\rEnter an integer value between 1 and 5");
     }
 }
 
-bool pauseCode = true;
-while (pauseCode == true) ;
+// Display the sum and then pause.
+Console.WriteLine($"\n\rThe sum of numbers {startIndex} through {numbers.Length} is: {SumValues(numbers, startIndex - 1)}");
 
-static void LoadProducts(string[,] products, int productCount)
+Console.WriteLine("press Enter to exit");
+_ = Console.ReadLine();
+
+// This method returns the sum of elements n through 5
+static int SumValues(int[] numbers, int n)
 {
-    Random rand = new();
+    int sum = 0;
+    for (int i = n; i < numbers.Length; i++)
+        sum += numbers[i];
 
-    for (int i = 0; i < productCount; i++)
-    {
-        int num1 = rand.Next(1, 10000) + 10000;
-        int num2 = rand.Next(1, 101);
-
-        string prodID = num1.ToString();
-
-        if (num2 < 91)
-        {
-            products[i, 1] = "existing";
-        }
-        else if (num2 == 91)
-        {
-            products[i, 1] = "new";
-            prodID += "-n";
-        }
-        else
-        {
-            products[i, 1] = "obsolete";
-            prodID += "-0";
-        }
-
-        products[i, 0] = prodID;
-    }
-}
-
-static string Process1(string[,] products, int item)
-{
-    Console.WriteLine($"Process1 message - working on {products[item, 1]} product");
-
-    return products[item, 1];
-}
-
-static string Process2(string[,] products, int item)
-{
-    Console.WriteLine($"Process2 message - working on product ID #: {products[item, 0]}");
-    if (products[item, 1] == "new")
-        Process3(products, item);
-
-    return "continue";
-}
-
-static void Process3(string[,] products, int item)
-{
-    Console.WriteLine($"Process3 message - processing product information for 'new' product");
+    return sum;
 }
