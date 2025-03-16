c840ae43684cdbe741a31152f9e760960795e2c9
Edson Samuel Jr <samuedson@gmail.com>
2025-03-16 16:29:16 -0300

    chore: configure conditional breakpoints in c#

--- Program.cs
+++ Program.cs
@@ -1,26 +1,69 @@
-﻿/* 
-This code uses a names array and corresponding methods to display
-greeting messages
-*/
+﻿int productCount = 2000;
+string[,] products = new string[productCount, 2];
 
-string[] names = ["Sophia", "Andrew", "AllGreetings"];
+LoadProducts(products, productCount);
 
-string messageText = "";
-
-foreach (string name in names)
+for (int i = 0; i < productCount; i++)
 {
-    if (name == "Sophia")
-        messageText = SophiaMessage();
-    else if (name == "Andrew")
-        messageText = AndrewMessage();
-    else if (name == "AllGreetings")
-        messageText = SophiaMessage() + "\n\r" + AndrewMessage();
+    string result;
+    result = Process1(products, i);
 
-    Console.WriteLine(messageText + "\n\r");
+    if (result != "obsolete")
+    {
+        _ = Process2(products, i);
+    }
 }
 
 bool pauseCode = true;
-while (pauseCode == true);
+while (pauseCode == true) ;
 
-static string SophiaMessage() => "Hello, my name is Sophia.";
-static string AndrewMessage() => "Hi, my name is Andrew. Good to meet you.";
+static void LoadProducts(string[,] products, int productCount)
+{
+    Random rand = new();
+
+    for (int i = 0; i < productCount; i++)
+    {
+        int num1 = rand.Next(1, 10000) + 10000;
+        int num2 = rand.Next(1, 101);
+
+        string prodID = num1.ToString();
+
+        if (num2 < 91)
+        {
+            products[i, 1] = "existing";
+        }
+        else if (num2 == 91)
+        {
+            products[i, 1] = "new";
+            prodID += "-n";
+        }
+        else
+        {
+            products[i, 1] = "obsolete";
+            prodID += "-0";
+        }
+
+        products[i, 0] = prodID;
+    }
+}
+
+static string Process1(string[,] products, int item)
+{
+    Console.WriteLine($"Process1 message - working on {products[item, 1]} product");
+
+    return products[item, 1];
+}
+
+static string Process2(string[,] products, int item)
+{
+    Console.WriteLine($"Process2 message - working on product ID #: {products[item, 0]}");
+    if (products[item, 1] == "new")
+        Process3(products, item);
+
+    return "continue";
+}
+
+static void Process3(string[,] products, int item)
+{
+    Console.WriteLine($"Process3 message - processing product information for 'new' product");
+}
