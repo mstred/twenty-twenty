0f8129bdba5b2933d5f0002775504bf1ed323820
Edson Samuel Jr <samuedson@gmail.com>
2025-04-19 11:14:23 -0300

    chore: catch specific exception types, pt.2
    forcing OverflowException through overflown type conversion on a checked block

--- Program.cs
+++ Program.cs
@@ -26,8 +26,13 @@ static void WriteMessage()
     double float1 = 3000.0;
     double float2 = 0.0;
     int number1 = 3000;
-    int number2 = 0;
+    // int number2 = 0;
 
     Console.WriteLine(float1 / float2);
-    Console.WriteLine(number1 / number2);
+    // Console.WriteLine(number1 / number2);
+
+    checked
+    {
+        byte smallNumber = (byte)number1;
+    }
 }
